4facefa89f780a008441e2007e43471d555c5aba
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-01-13 10:41:46 +0330

    refactore reducer with createReducer

--- src/Redux/store/todos.js
+++ src/Redux/store/todos.js
@@ -1,21 +1,33 @@
-import { createAction } from "@reduxjs/toolkit"
+import { createAction, createReducer } from "@reduxjs/toolkit"
 
 export const addTodo = createAction('ADD_TODO');
 export const removeTodo = createAction('REMOVE_TODO');
 export const doTodo = createAction('DO_TODO');
 
-// Reducer
-export default function reducer (state = [] , action) {
-    switch (action.type) {
-        case addTodo.type: {
-            return [...state,action.payload]
-        }
-        case removeTodo.type : {
-            return state.filter(todo => todo.id !== action.payload.id)
-        }
-        default: {
-            return state ;
-        }
+export default createReducer([], {
+    [addTodo.type]: (state, action) => {
+        state.push(action.payload)
+    },
+    [removeTodo.type]: (state, action) => {
+        //coding
+    },
+    [addTodo.type]: (state, action) => {
+        //coding
     }
-}
+})
+
+// Reducer
+// export default function reducer(state = [], action) {
+//     switch (action.type) {
+//         case addTodo.type: {
+//             return [...state, action.payload]
+//         }
+//         case removeTodo.type: {
+//             return state.filter(todo => todo.id !== action.payload.id)
+//         }
+//         default: {
+//             return state;
+//         }
+//     }
+// }
 
